e5beacfb7351648cc7fbbb5ed00ebc6809b86a68
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-09 14:36:18 -0300

    main

--- src/views/TableList.js
+++ src/views/TableList.js
@@ -37,45 +37,24 @@ function TableList() {
                   <tbody>
                     <tr>
                       <td>1</td>
-                      <td>Dakota Rice</td>
-                      <td>$36,738</td>
-                      <td>Niger</td>
-                      <td>Oud-Turnhout</td>
+                      <td>Controle de Frota</td>
                     </tr>
                     <tr>
                       <td>2</td>
-                      <td>Minerva Hooper</td>
-                      <td>$23,789</td>
-                      <td>Curaçao</td>
-                      <td>Sinaai-Waas</td>
+                      <td>Controle de OS</td>
+
                     </tr>
                     <tr>
                       <td>3</td>
-                      <td>Sage Rodriguez</td>
-                      <td>$56,142</td>
-                      <td>Netherlands</td>
-                      <td>Baileux</td>
+                      <td>CAT</td>
                     </tr>
                     <tr>
                       <td>4</td>
-                      <td>Philip Chaney</td>
-                      <td>$38,735</td>
-                      <td>Korea, South</td>
-                      <td>Overland Park</td>
+                      <td>PGR PCMSO</td>
                     </tr>
                     <tr>
                       <td>5</td>
-                      <td>Doris Greene</td>
-                      <td>$63,542</td>
-                      <td>Malawi</td>
-                      <td>Feldkirchen in Kärnten</td>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>Mason Porter</td>
-                      <td>$78,615</td>
-                      <td>Chile</td>
-                      <td>Gloucester</td>
+                      <td>E-Social</td>
                     </tr>
                   </tbody>
                 </Table>
@@ -96,53 +75,29 @@ function TableList() {
                     <tr>
                       <th className="border-0">ID</th>
                       <th className="border-0">Name</th>
-                      <th className="border-0">Salary</th>
-                      <th className="border-0">Country</th>
-                      <th className="border-0">City</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
+                  <tr>
                       <td>1</td>
-                      <td>Dakota Rice</td>
-                      <td>$36,738</td>
-                      <td>Niger</td>
-                      <td>Oud-Turnhout</td>
+                      <td>Controle de Frota</td>
                     </tr>
                     <tr>
                       <td>2</td>
-                      <td>Minerva Hooper</td>
-                      <td>$23,789</td>
-                      <td>Curaçao</td>
-                      <td>Sinaai-Waas</td>
+                      <td>Controle de OS</td>
+
                     </tr>
                     <tr>
                       <td>3</td>
-                      <td>Sage Rodriguez</td>
-                      <td>$56,142</td>
-                      <td>Netherlands</td>
-                      <td>Baileux</td>
+                      <td>CAT</td>
                     </tr>
                     <tr>
                       <td>4</td>
-                      <td>Philip Chaney</td>
-                      <td>$38,735</td>
-                      <td>Korea, South</td>
-                      <td>Overland Park</td>
+                      <td>PGR PCMSO</td>
                     </tr>
                     <tr>
                       <td>5</td>
-                      <td>Doris Greene</td>
-                      <td>$63,542</td>
-                      <td>Malawi</td>
-                      <td>Feldkirchen in Kärnten</td>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>Mason Porter</td>
-                      <td>$78,615</td>
-                      <td>Chile</td>
-                      <td>Gloucester</td>
+                      <td>E-Social</td>
                     </tr>
                   </tbody>
                 </Table>
